e5bc6a15969e5d537b354f98ae000b0bf94385d9
Reinhardt <reinhardt@teasy.at>
2022-10-06 14:20:49 +0200

    Show raster tasks in screen 06

--- src/main/kotlin/at/duk/services/RasterUpload.kt
+++ src/main/kotlin/at/duk/services/RasterUpload.kt
@@ -1,54 +1,96 @@
 package at.duk.services
 
 import at.duk.tables.TableRasterTasks
-import at.duk.tables.TableUploadedRasterData
+import io.ktor.server.config.*
+import koodies.exec.Process
+import koodies.exec.error
+import koodies.exec.exitCode
+import koodies.shell.ShellScript
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
+import java.nio.file.Paths
 
 class RasterUpload {
     companion object {
         private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        fun uploadIntoRasterTasks(fileDescription: String, fileName: String, cachePath: File) {
-            /*2.	set values in table uploaded_raster_data
-            3.	set values in table raster_tasks
-            4.	Run "raster2pgsql" - Import Skript - create temporary table name, update raster_tasks
-            temporary table "rd_126172617" is created*/
 
-            lateinit var id: EntityID<Int>
+        private const val windowsScript = "\"%postgresqlBinDirectory%\\raster2pgsql\" -b 1 -F -I -C -s 4326 " +
+                "\"%uploadDirectory%\\%tifFileName%\" public.%tableName% > %uploadDirectory%\\rasterImport.sql\n" +
+                "\"%postgresqlBinDirectory%\\psql\" -f %uploadDirectory%\\rasterImport.sql %connection%"
 
-            val tmpTableName = "table" + (1..12)
-                .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
-                .map(charPool::get)
-                .joinToString("")
+        fun uploadIntoRasterTasks(
+            fileName: String,
+            config: ApplicationConfig,
+            tmpName: String,
+            rasterTasksId: EntityID<Int>
+        ) {
+            try {
+                ShellScript(generateScripts(config, tmpName, fileName, rasterTasksId.value)).exec.async().also {
+                    val pidV = it.pid
+                    transaction {
+                        TableRasterTasks.update({ TableRasterTasks.id eq rasterTasksId }) { stmt ->
+                            stmt[pid] = pidV.toInt()
+                        }
+                    }
+                    it.waitFor()
 
-            transaction {
-                addLogger(StdOutSqlLogger)
-                /*val id = Packages.insertAndGetId {
-                    it[name] = "Hugo2"
-                    it[created] = CurrentDateTime
+                    if (it.state is Process.State.Exited.Failed) {
+                        transaction {
+                            TableRasterTasks.update({ TableRasterTasks.id eq rasterTasksId }) { table ->
+                                table[rc] = it.exitCode
+                                table[message] = it.error
+                                table[end] = CurrentDateTime
+                            }
+                        }
+                    } else {
+                        transaction {
+                            TableRasterTasks.update({ TableRasterTasks.id eq rasterTasksId }) { table ->
+                                table[rc] = it.exitCode
+                                table[message] = "Terminated successfully"
+                                table[end] = CurrentDateTime
+                            }
+                        }
+                    }
                 }
-                println("ID: ----------------- $id")*/
-
-                id = TableUploadedRasterData.insertAndGetId {
-                    it[filename] = fileName
-                    it[name] = fileDescription
-                    it[tmpTable] = tmpTableName
-                }
-            }
-            // start script
-            val scriptPid = 4711
-
-            transaction {
-                addLogger(StdOutSqlLogger)
-                TableRasterTasks.insert {
-                    it[pid] = 4711
-                    it[start] = CurrentDateTime
-                    it[uploadedRasterDataId] = id.value
+            } catch (ex: Exception) {
+                transaction {
+                    TableRasterTasks.update({ TableRasterTasks.id eq rasterTasksId }) { table ->
+                        table[rc] = -1
+                        table[message] = ex.message?:"Undefined Error!"
+                        table[end] = CurrentDateTime
+                    }
                 }
             }
         }
+
+        private fun generateScripts(config: ApplicationConfig, tmpName: String, fileName: String, id: Int): String {
+            val dataCacheDirectory = config.propertyOrNull("dataCache.directory")?.getString() ?: Paths.get("").toAbsolutePath().toString()
+            val jobsPath = File(dataCacheDirectory).resolve("rasterData").resolve("uploads").resolve(tmpName)
+
+            val conn = (config.propertyOrNull("ktor.database.connection.jdbc")?.getString() ?: "").split("//")[1]
+            val user = config.propertyOrNull("ktor.database.connection.user")?.getString() ?: "not configured"
+            val passwd = config.propertyOrNull("ktor.database.connection.user")?.getString() ?: "not configured"
+            val connStr = "postgresql://$user:$passwd@$conn"
+
+            val fileContent = windowsScript.replace("%postgresqlBinDirectory%", config.propertyOrNull("ktor.database.postgresqlBinDirectory")?.getString() ?:"")
+                .replace("%uploadDirectory%", jobsPath.toString())
+                .replace("%tifFileName%", fileName)
+                .replace("%tableName%", "table_$tmpName")
+                .replace ("%connection%", connStr)
+                .replace ("%id%", id.toString())
+
+            jobsPath.resolve("import.bat").writeText(fileContent)
+            // check for OS
+            return jobsPath.resolve("import.bat").toString()
+        }
+
+        fun genTempName() = (1..12)
+            .map { kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+
     }
 }
